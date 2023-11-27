dfdb3c42257e479af5f528a8f820c4940ec55e27
Rajan Kumar <106719815+iamrajankumar@users.noreply.github.com>
2023-11-27 23:08:47 +0530

    fist Assignment
    My first assignment in JSK class

---
+++ script.js
@@ -0,0 +1,177 @@
+
+
+//        Q1.Arithmetic Operators:
+
+//A. Write a JavaScript program that takes two numbers as input and performs the following arithmetic operations:
+//a. Addition
+
+let add;
+
+add = 5 + 7;
+
+document.write(add);
+document.write("<br>")
+
+// b. Subtraction
+
+let sub;
+
+sub = 20 - 6;
+
+document.write(sub);
+document.write("<br>")
+
+// c. Multiplication
+
+let mul;
+
+mul = 10 * 5;
+
+document.write(mul);
+document.write("<br>")
+
+// d. Division
+
+let div;
+
+div = 50 / 5;
+
+document.write(div);
+document.write("<br>")
+
+//      Q2 Remainder Operator:
+
+// Extend the program to include the calculation of the remainder when one number is divided by another.
+
+let remainder;
+
+remainder = 52 % 3;
+
+document.write(remainder);
+
+let a;
+a = 95;
+let b;
+b = 2;
+let z = a % b;
+
+console.log(z);
+
+//       Q3 Assignment Operators:
+
+// A. Create a variable and assign it an initial value.
+
+var nam;//Declare  variable
+
+nam = "rocky";//Assign the value "rocky" to nam
+
+document.write(nam);
+
+let num; //Declare  variable
+
+num = 20; //Assign the value 20 to num
+
+document.write(num);
+
+let add1;//Declare  variable
+
+add1 = nam + num;//Assign the value
+
+document.write(add1);
+
+//      Q4 Unary Operators:
+
+// A. Implement a section in your program to demonstrate the use of unary operators.
+
+//    a. Use the unary plus operator to convert a string to a number.
+
+let name1 = "Rakesh";
+
+console.log(typeof +name1);
+
+let number = "10";
+
+document.write(typeof +number);
+
+//    b. Use the unary minus operator to negate a numeric value.
+
+let x = 10;
+
+x = -x;
+
+console.log(x);
+
+let y;
+y = "40";
+
+console.log(-y);
+
+//      c .Use the logical NOT operator to invert a boolean value
+
+
+let d = 5;
+let s = 10;
+let k = d != s;
+console.log(k);
+
+let f = 20;
+let e = 20;
+let u = f != e;
+console.log(u);
+
+//     Q5 Comparison Operators:
+
+// A. Write a program that compares two numbers and prints the following comparisons:
+
+//   a. Equal to
+
+let m = 10;
+let m1 = 10;
+let equal = m == m1;
+console.log(equal);
+
+let l = 10;
+let l1 = 20;
+let equal1 = l == l1;
+console.log(equal1);
+
+//   b. Not equal to
+
+let p = 15;
+let p1 = 13;
+let not = p != p1;
+console.log(not);
+
+// c. Greater than
+
+let bb = 24;
+let b1 = 20;
+let b2 = 24 > 20;
+console.log(b2);
+
+// d. Less than
+
+let ff = 19;
+let ff1 = 20;
+let ff2 = 19 < 20;
+console.log(ff2);
+
+// e. Greater than or equal to
+
+let gr = 22;
+let eq = 20;
+let greq = 22 >= 20;
+console.log(greq);
+
+// f. Less than or equal to
+
+let les = 12;
+let eq1 = 20;
+let less = 12 <= 20;
+console.log(less);
+
+
+
+
+
+
